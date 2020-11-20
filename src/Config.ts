ab1cdd9635ee58170c9328e782e82243b1396bb9
bluebeargreen <bluebeardiscord@gmail.com>
2020-11-20 15:46:06 -0600

    Readable config for those who need it

--- src/Config.ts
+++ src/Config.ts
@@ -1,4 +1,4 @@
-export let token: string = ""; //paste you bot token here
-export let prefix: string = "/";
-export let owners: string[] = [""]; //place your discord ID here
-export let dbName: string = "TutorialDB"
+export let token: string = ""; //paste you bot token here, To get you discord bot token go here: https://discord.com/developers/applications
+export let prefix: string = "/"; //change your prefix, The default is "/", but you can change this to whatever you want it to be
+export let owners: string[] = [""]; //place your discord ID here, This is not needed but you can place it here
+export let dbName: string = "MainDB" //SQL database name, You do not need to change this
